5458b17e3547e4b6fa252546b100a082b6c97d61
Matheus E. Muller <hello@memuller.com>
2015-02-05 18:45:57 -0200

    Adds file for what-to-do discussion.

---
+++ Python/00-whatnow.py
@@ -0,0 +1,11 @@
+# Foco e frequência
+# Actually seguir horários
+
+# Generalizar e entender o que aprende
+# Pensar nos 'truques' que sabe
+# Quebrar o programa em vários pedaços até conseguir fazer
+# Teste cada pedaço antes do próximo
+# Não mude o que está funcionando
+
+# Actual programming
+# presente? hint coins?
